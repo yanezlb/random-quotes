6d312c70b84e4e9a6fd07ea0ef297e1fe2977831
Luis Yanez <yanezblancoluis@gmail.com>
2021-03-15 23:11:47 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Container, Row, Col, Alert } from 'react-bootstrap';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs lg="2">
+            1 of 3
+          </Col>
+          <Col md="auto">Variable width content</Col>
+          <Col xs lg="2">
+            3 of 3
+          </Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col md="auto">Variable width content</Col>
+          <Col xs lg="2">
+            3 of 3
+          </Col>
+        </Row>
+      </Container>
+      
+        <Alert variant="light">
+          This is a asdasd alert—check it out!
+        </Alert>
+      
     </div>
   );
 }
